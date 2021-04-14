b1dd4ffd7524e2b97185354cef6ebd0bc484a080
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2021-04-14 22:32:48 +0800

    update files

--- syn_by_git.py
+++ syn_by_git.py
@@ -4,3 +4,17 @@
 # @Email  : linbeyoung@stu.pku.edu.cn
 # @File   : syn_by_git.py
 
+import os
+
+root_path = 'fakepages_data/book_pages/'
+folders = os.listdir(root_path)
+
+for folder in folders:
+    if os.path.isdir(os.path.join(root_path, folder)):
+        for num in range(20):
+            file_name = 'book_page_' + str(num) + '.jpg'
+            file_path = os.path.join(root_path, folder, 'imgs_v', file_name)
+            try:
+                os.system('git add ' + file_path)
+            except:
+                print('error: ' + file_path)
