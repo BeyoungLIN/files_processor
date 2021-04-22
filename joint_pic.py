d6925b695131f5a4b2d3aaf9ffc4ccaf83a15b45
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2021-04-23 02:28:33 +0800

    support multi folders

--- joint_pic.py
+++ joint_pic.py
@@ -15,12 +15,14 @@ def joint_vertical(ims_list, res_name):  # 传递一个ims列表和保存名
     ims = [Image.open(img) for img in ims_list]
 
     # 图片转化为相同的尺寸
-    # height1 =
-
-    # ims = []
-    # for i in im_list:
-    #     new_img = i.resize((1480, 990), Image.BILINEAR)
-    # ims.append(new_img)
+    # if RESIZE == 1:
+    #
+    #     height1 =
+    #
+    #     ims = []
+    #     for i in im_list:
+    #         new_img = i.resize((1480, 990), Image.BILINEAR)
+    #     ims.append(new_img)
 
     # 单幅图像尺寸
     width, height = ims[0].size
@@ -29,8 +31,7 @@ def joint_vertical(ims_list, res_name):  # 传递一个ims列表和保存名
     new_img = Image.new(ims[0].mode, (width, height * len(ims)))
     '''
     todolist:
-    1. 对于不同尺寸的图片进行兼容,选择是否统一格式
-    2. 对图片数量进行兼容
+    1. 对于不同尺寸的图片进行兼容,选择是否统一格式 if resize = 1
     '''
 
     # 拼接图片
@@ -43,15 +44,24 @@ def joint_vertical(ims_list, res_name):  # 传递一个ims列表和保存名
 
 if __name__ == '__main__':
 
+
+    folders_list = [
+        '../AC_OCR/Dingxiu_test/Dingxiu_2/Dingxiu_2_demo_results_total50',
+        '../AC_OCR/Dingxiu_test/Dingxiu_2/Dingxiu_2_demo_results_res50',
+        '../AC_OCR/Dingxiu_test/Dingxiu_2/Dingxiu_2_demo_results_newdb'
+    ]
+
+    files = os.listdir(folders_list[0])
+
+    for file in files:
+        if
+
     for id_num in range(1, 785):
         try:
             imgs = []
             img_name = str(id_num).zfill(6)
            # '''
-            folders_list = [
-                '../AC_OCR/Dingxiu_test/Dingxiu_2/Dingxiu_2_demo_results_res50'
-                '../AC_OCR/Dingxiu_test/Dingxiu_2/Dingxiu_2_demo_results_newdb'
-            ]
+
             for folder in folders_list:
                 pic_name = os.path.join(folder, img_name + '.jpg')
                 imgs.append(pic_name)
@@ -65,7 +75,7 @@ if __name__ == '__main__':
             imgs.append(pic2)
             '''
 
-            res_path = '../AC_OCR/joint_compare/Dingxiu_2/res50_2.0_3.5'
+            res_path = '../AC_OCR/joint_compare/Dingxiu_2/total_res502.0_res503.5'
             if not os.path.exists(res_path):
                 os.mkdir(res_path)
             joint_vertical(imgs, os.path.join(res_path, img_name + '_joint.jpg'))
