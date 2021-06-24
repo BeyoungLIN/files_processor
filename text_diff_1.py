98dcc573d5c7e78df04d7889a822fc8fe4935a8e
beyoung <linbeyoung@foxmail.com>
2021-06-24 23:45:00 +0800

    find text diff

--- text_diff_1.py
+++ text_diff_1.py
@@ -5,14 +5,20 @@
 # @File   : text_diff_1.py
 
 import difflib
+
+
 def diff(fn1, fn2):
     """对比两个文件内容的不同并以html的格式返回"""
     with open(fn1) as f1: content1 = f1.readlines()
     with open(fn2) as f2: content2 = f2.readlines()
     df = difflib.HtmlDiff()
     return df.make_file(content1, content2)
+
+
 if __name__ == '__main__':
-    fn1 = 'doc/passwd'
-    fn2 = 'doc/passwd.bak'
+    fn1 = '/Users/Beyoung/Desktop/Projects/AC_OCR/temp/史记1_resapi_mix.txt'
+    fn2 = '/Users/Beyoung/Desktop/Projects/AC_OCR/temp/史记1_res_recog_adv.txt'
     res = diff(fn1, fn2)
+    with open ('/Users/Beyoung/Desktop/Projects/AC_OCR/temp/WX20210623-230418@2x_res_recog_diff.html', 'w') as f:
+        f.write(res)
     print(res)
